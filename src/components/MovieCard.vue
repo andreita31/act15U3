c4146959ff52b988070405a6fbeb9d8dee06cfce
Andrea Rodriguez <andrear_20@alu.uabcs.mx>
2023-10-02 12:52:53 -0700

    Creado componente para pelicula

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,54 @@
+<script setup>
+defineProps({
+    img: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: false,
+        default: -1
+    }
+})
+</script>
+<template>
+    <div class="movie-container">
+        <div class="img-cont">
+            <img :src="img">
+        </div>
+        <span class="title">{{ title }}</span>
+        <span class="rating">{{ rating < 0 ? "Sin valoraciones" : `${rating}/10`}}</span>
+    </div>
+</template>
+
+<style>
+    .movie-container{
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        overflow: hidden;
+    }
+
+    img{
+        width: 100%;
+    }
+
+    .title{
+        margin-top: .5em;
+        text-align: center;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        overflow: hidden;
+        max-width: 100%;
+        font-weight: 700;
+        color: #f2f2f2;
+    }
+
+    .rating{
+        color: rgb(233, 218, 12);
+    }
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<script setup>
+import MovieCard from '../components/MovieCard.vue';
+</script>
+
+<template>
+  <main>
+    <MovieCard img="https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg" title="Título jivhg gvytvytfytf yf6rffytfy tfutg7ygug7g7g7g7yguyguyg 6u gu gy"></MovieCard>
+    <MovieCard img="https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg" title="Título jivhg gvytvytfytf yf6rffytfy tfutg7ygug7g7g7g7yguyguyg 6u gu gy"></MovieCard>
+    <MovieCard img="https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg" title="Título jivhg gvytvytfytf yf6rffytfy tfutg7ygug7g7g7g7yguyguyg 6u gu gy"></MovieCard>
+    <MovieCard img="https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg" title="Título jivhg gvytvytfytf yf6rffytfy tfutg7ygug7g7g7g7yguyguyg 6u gu gy"></MovieCard>
+    <MovieCard img="https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg" title="Título jivhg gvytvytfytf yf6rffytfy tfutg7ygug7g7g7g7yguyguyg 6u gu gy"></MovieCard>
+    <MovieCard img="https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg" title="Título jivhg gvytvytfytf yf6rffytfy tfutg7ygug7g7g7g7yguyguyg 6u gu gy"></MovieCard>
+    <MovieCard img="https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg" title="Título jivhg gvytvytfytf yf6rffytfy tfutg7ygug7g7g7g7yguyguyg 6u gu gy"></MovieCard>
+    <MovieCard img="https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg" title="Título jivhg gvytvytfytf yf6rffytfy tfutg7ygug7g7g7g7yguyguyg 6u gu gy"></MovieCard>
+  </main>
+</template>
+
+<style>
+  main{
+    gap: 1.5em;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    max-width: 960px;
+    margin: auto;
+  }
+</style>
